40e5b983058bf3553e61a9e06b96c967479ce700
Patrick J <patrick.pddstudio@googlemail.com>
2017-02-20 20:16:37 +0100

    Added build script

---
+++ build.sh
@@ -0,0 +1,3 @@
+ #!/bin/bash
+echo "Building vbuild..."
+valac --pkg json-glib-1.0 --pkg gee-0.8 --pkg glib-2.0 src/ValaBuild.vala src/FilesProcessor.vala src/ConfigParser.vala src/BuildConfig.vala src/CommandBuilder.vala -o vbuild
